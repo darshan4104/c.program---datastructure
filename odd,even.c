54cbfef0839ee506f94fbce527678c152c2bbbaa
darshan4104 <darshanthalwar2002@gmail.com>
2024-09-10 03:09:49 -0700

    Create odd,even.c

---
+++ odd,even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int darshan;
+  printf("Enter the number");
+  scanf("%d", &darshan);
+  if (darshan%2==0) {
+    printf("even");
+  }
+  else {
+    printf("odd");
+  }
+}
