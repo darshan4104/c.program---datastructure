54c86c188688361021785d1ec80363ac88965b81
darshan4104 <darshanthalwar2002@gmail.com>
2024-09-13 09:26:56 +0530

    Create Dynamic initialization.c

---
+++ Dynamic initialization.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  int arr[5] = {1, 2, 3, 4, 5}; // initialization
+  int = 5;
+  int*arr = (int*)malloc(n * sizeof(int)); // dynamic memory allocation
+  int firstElement = arr[0]; // accessing the first element
+  arr[2] = 10; // modifying the third element
+  for(int i=0; i<5; i++) {
+    printf("%d", arr[i]);
+  }
+  return 0;
+}
